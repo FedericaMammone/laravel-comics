b5599a2ce442a08f1bb0f1ab52c5130062003cf5
Fef97 <federicakr97@gmail.com>
2023-07-15 12:16:02 +0200

    aggiunte immagini fumetti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,16 @@
 
 @section('content')
     <div class="container-home">
-        <div class="row">
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
+        <div class="row justify-content-between flex-wrap">
+            @foreach ($fumetti as $fumetto)
+                <div class="col-2 p-4">
+                    <img src="{{ $fumetto['thumb'] }}" alt="">
+                    <h4>{{ $fumetto['title'] }}</h4>
+                </div>
+            @endforeach
+
+
         </div>
-        <div class="row">
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-            <div class="card">Card</div>
-        </div>
+
     </div>
 @endsection
